2cdd7618c130412d37d2c940151e02871c1d833f
Travis Bowen <Travisbowen23@gmail.com>
2018-09-20 17:22:49 -0700

    Initial Commit

---
+++ RunningMan WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  RunningMan WatchKit Extension
+//
+//  Created by Travis Bowen on 9/20/18.
+//  Copyright © 2018 UpscaleApps. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ RunningMan/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RunningMan
+//
+//  Created by Travis Bowen on 9/20/18.
+//  Copyright © 2018 UpscaleApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
